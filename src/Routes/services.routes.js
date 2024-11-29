ab76cf59b586973e49434344d17d3b565e056a04
amax2019189 <amax-2019189@kinal.edu.gt>
2024-11-28 19:52:59 -0600

    update and delete services

--- src/Routes/services.routes.js
+++ src/Routes/services.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { check } from "express-validator";
 import { validarCampos } from "../middlewares/validar-campos.js";
 import { validarJWT } from "../middlewares/validar-jwt.js";
-import { createService, getService } from "../Controllers/services.controller.js";
+import { createService, deleteService, getService, updateService } from "../Controllers/services.controller.js";
 
 const router = Router();
 
@@ -25,6 +25,27 @@ router.get(
         validarJWT,
     ],
     getService
+);
+
+router.put(
+    "/:id",
+    [
+        check("name"),
+        check("description"),
+        check("price"),
+        check("category"),
+        validarCampos,
+        validarJWT,
+    ],
+    updateService
+);
+
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+    ],
+    deleteService
 );
 
 export default router;
