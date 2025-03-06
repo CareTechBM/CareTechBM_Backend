bc896a7c2c6edb449cba9740ce507e704f1464cf
bmendoza2019349 <bmendoza-2019349@kinal.edu.gt>
2025-03-06 13:51:51 -0600

    cambios

--- configs/server.js
+++ configs/server.js
@@ -5,7 +5,6 @@ import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
 import authRoutes from '../src/routes/users.routes.js'
 import apiLimiter from "../src/middlewares/validar-peticiones.js";
-import quotesRoutes from '../src/routes/quote.routes.js';
 import filesRoutes from '../src/routes/files.routes.js';
 import serviceRoutes from '../src/routes/services.routes.js';
 import prescriptionRoutes from '../src/routes/prescriptions.routes.js';
@@ -14,6 +13,7 @@ import doctorsRoutes from '../src/routes/doctor.routes.js';
 import patienteRoutes from '../src/routes/patient.routes.js';
 import medicineRoutes from '../src/routes/medication.routes.js';
 import categoryRoutes from '../src/routes/categorie.routes.js';
+import quotesRoutes from '../src/routes/quote.routes.js';
 
 class Server {
     constructor() {
